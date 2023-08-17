29aee54c1cb9c11f08c8ccb174da325191ce425b
Mark Vrijlandt <mark.vrijlandt@tno.nl>
2023-08-17 09:09:13 +0200

    added job_name and work_flow_type to job datatable, updated .env

--- postgres_db/initdb.d/init.sql
+++ postgres_db/initdb.d/init.sql
@@ -4,10 +4,13 @@
 
 --CREATE DATABASE nieuwewarmtenu;
 
+CREATE TYPE work_flow_type AS ENUM ('growth_optimizer');
 CREATE TYPE job_status AS ENUM ('registered', 'running', 'finished', 'error');
 
 CREATE TABLE jobs (
     job_id uuid PRIMARY KEY,
+    job_name text NOT NULL,
+    work_flow_type work_flow_type NOT NULL,
     map_editor_user text,
     status job_status NOT NULL,
     input_config text,
