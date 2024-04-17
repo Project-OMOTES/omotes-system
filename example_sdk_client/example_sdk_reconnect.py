96b5fcb6a5d1d11500711f2a7c28f81f9869e7a3
Sebastiaan la Fleur <sebastiaan.lafleur@tno.nl>
2024-04-17 12:24:44 +0200

    mvp.1: Update example_sdk_client to new sdk version and update orchestrator and optimizer worker.

--- example_sdk_client/example_sdk_reconnect.py
+++ example_sdk_client/example_sdk_reconnect.py
@@ -9,7 +9,7 @@ from omotes_sdk.omotes_interface import (
     JobProgressUpdate,
     JobStatusUpdate,
 )
-from omotes_sdk.workflow_type import WorkflowType
+from omotes_sdk.workflow_type import WorkflowType, WorkflowTypeManager
 
 rabbitmq_config = RabbitMQConfig(username="omotes", password="somepass1", virtual_host="omotes")
 
@@ -37,15 +37,18 @@ def handle_on_progress_update(job: Job, progress_update: JobProgressUpdate):
 
 
 try:
-    omotes_if = OmotesInterface(rabbitmq_config)
+    workflow_simulator = WorkflowType("simulator", "some descr")
+    workflow_manager = WorkflowTypeManager([workflow_simulator])
+
+    omotes_if = OmotesInterface(rabbitmq_config, possible_workflows=workflow_manager)
     omotes_if.start()
 
     omotes_if.connect_to_submitted_job(
-        job=Job(id=UUID("81b1fec2-60b0-468a-804f-fc06c533fbb2"), workflow_type=WorkflowType("grow_simulator", "some descr")),
+        job=Job(id=UUID("81b1fec2-60b0-468a-804f-fc06c533fbb2"), workflow_type=workflow_simulator),
         callback_on_finished=handle_on_finished,
         callback_on_progress_update=handle_on_progress_update,
         callback_on_status_update=handle_on_status_update,
-        auto_disconnect_on_result=True
+        auto_disconnect_on_result=True,
     )
     time.sleep(60)
 finally:
