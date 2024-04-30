7bc5e3d05e9ba8501d5c182ab9b50bd0a13381db
Sebastiaan la Fleur <sebastiaan.lafleur@tno.nl>
2024-04-30 14:07:20 +0200

    42: Each setup script for component now expects the env file path and docker compose file path as arguments. Also cleanup the scripts.

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
-./scripts/setup_orchestrator_postgres_db.sh
-./scripts/setup_rabbitmq.sh
-./scripts/setup_influxdb.sh
+ENV_FILE="./.env"
+DOCKER_COMPOSE_FILE="./docker-compose.yml"
+
+./scripts/setup_orchestrator_postgres_db.sh ${1} ${2}
+./scripts/setup_rabbitmq.sh ${1} ${2}
+./scripts/setup_influxdb.sh ${1} ${2}
--- scripts/setup_rabbitmq.sh
+++ scripts/setup_rabbitmq.sh
@@ -1,14 +1,16 @@
 #!/bin/bash
-. scripts/_select_docker_compose.sh
-. scripts/_load_dot_env.sh .env
+. "$(dirname "$0")"/_select_docker_compose.sh
+. "$(dirname "$0")"/_load_dot_env.sh $1
 
-$DOCKER_COMPOSE up -d --wait rabbitmq
-$DOCKER_COMPOSE exec rabbitmq rabbitmqctl add_vhost omotes
-$DOCKER_COMPOSE exec rabbitmq rabbitmqctl set_permissions --vhost omotes root ".*" ".*" ".*"
-$DOCKER_COMPOSE exec rabbitmq rabbitmqctl add_vhost omotes_celery
-$DOCKER_COMPOSE exec rabbitmq rabbitmqctl set_permissions --vhost omotes_celery root ".*" ".*" ".*"
+$DOCKER_COMPOSE -f ${2} up -d --wait rabbitmq
 
-$DOCKER_COMPOSE exec rabbitmq rabbitmqctl add_user --vhost omotes "${RABBITMQ_OMOTES_USER_NAME}" "${RABBITMQ_OMOTES_USER_PASSWORD}"
-$DOCKER_COMPOSE exec rabbitmq rabbitmqctl set_permissions --vhost omotes "${RABBITMQ_OMOTES_USER_NAME}" ".*" ".*" ".*"
-$DOCKER_COMPOSE exec rabbitmq rabbitmqctl add_user --vhost omotes_celery "${RABBITMQ_CELERY_USER_NAME}" "${RABBITMQ_CELERY_USER_PASSWORD}"
-$DOCKER_COMPOSE exec rabbitmq rabbitmqctl set_permissions --vhost omotes_celery "${RABBITMQ_CELERY_USER_NAME}" ".*" ".*" ".*"
+DOCKER_EXEC="$DOCKER_COMPOSE -f ${2} exec rabbitmq rabbitmqctl"
+${DOCKER_EXEC} add_vhost omotes
+${DOCKER_EXEC} set_permissions --vhost omotes root ".*" ".*" ".*"
+${DOCKER_EXEC} add_vhost omotes_celery
+${DOCKER_EXEC} set_permissions --vhost omotes_celery root ".*" ".*" ".*"
+
+${DOCKER_EXEC} add_user --vhost omotes "${RABBITMQ_OMOTES_USER_NAME}" "${RABBITMQ_OMOTES_USER_PASSWORD}"
+${DOCKER_EXEC} set_permissions --vhost omotes "${RABBITMQ_OMOTES_USER_NAME}" ".*" ".*" ".*"
+${DOCKER_EXEC} add_user --vhost omotes_celery "${RABBITMQ_CELERY_USER_NAME}" "${RABBITMQ_CELERY_USER_PASSWORD}"
+${DOCKER_EXEC} set_permissions --vhost omotes_celery "${RABBITMQ_CELERY_USER_NAME}" ".*" ".*" ".*"
