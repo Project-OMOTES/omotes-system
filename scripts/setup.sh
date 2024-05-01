a536fc0736ca4dd59a7b67c9a8300a172f60be3f
Sebastiaan la Fleur <sebastiaan.lafleur@tno.nl>
2024-05-01 18:14:10 +0200

    42: Fix setup script to set correct env file and docker compose file.

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -3,6 +3,6 @@
 ENV_FILE="./.env"
 DOCKER_COMPOSE_FILE="./docker-compose.yml"
 
-./scripts/setup_orchestrator_postgres_db.sh ${1} ${2}
-./scripts/setup_rabbitmq.sh ${1} ${2}
-./scripts/setup_influxdb.sh ${1} ${2}
+./scripts/setup_orchestrator_postgres_db.sh $ENV_FILE $DOCKER_COMPOSE_FILE
+./scripts/setup_rabbitmq.sh $ENV_FILE $DOCKER_COMPOSE_FILE
+./scripts/setup_influxdb.sh $ENV_FILE $DOCKER_COMPOSE_FILE
