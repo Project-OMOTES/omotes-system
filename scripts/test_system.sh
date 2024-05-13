caf5ef7381fc5df0308e33b317fad8b9c7e6b3b5
Sebastiaan la Fleur <sebastiaan.lafleur@tno.nl>
2024-05-13 11:33:29 +0200

    40: Rename system_tests docker compose to include override in the name for descriptiveness.

--- scripts/test_system.sh
+++ scripts/test_system.sh
@@ -3,6 +3,6 @@
 
 export COMPOSE_PROJECT_NAME=system_tests
 
-$DOCKER_COMPOSE -f docker-compose.yml -f system_tests/docker-compose.yml down -v
+$DOCKER_COMPOSE -f docker-compose.yml -f system_tests/docker-compose.override.yml down -v
 ./scripts/setup.sh
-$DOCKER_COMPOSE -f docker-compose.yml -f system_tests/docker-compose.yml up --build --abort-on-container-exit
+$DOCKER_COMPOSE -f docker-compose.yml -f system_tests/docker-compose.override.yml up --build --abort-on-container-exit
