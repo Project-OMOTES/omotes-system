b09c10402dd121ab8141663ae357b4080eb8fdc9
Sebastiaan la Fleur <sebastiaan.lafleur@tno.nl>
2024-05-14 11:44:19 +0200

    40: Alter system_tests project name to omotes_system_tests.

--- scripts/test_system.sh
+++ scripts/test_system.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 . scripts/_select_docker_compose.sh
 
-export COMPOSE_PROJECT_NAME=system_tests
+export COMPOSE_PROJECT_NAME=omotes_system_tests
 
 $DOCKER_COMPOSE -f docker-compose.yml -f system_tests/docker-compose.override.yml down -v
 ./scripts/setup.sh
