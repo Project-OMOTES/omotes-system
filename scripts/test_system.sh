20e9031270c3389353d32d7ec39c466db93fb896
Sebastiaan la Fleur <sebastiaan@lfse.nl>
2024-06-17 17:10:34 +0200

    48: Update system test to set log level to warning to quiet down the services as much as possible.

--- scripts/test_system.sh
+++ scripts/test_system.sh
@@ -3,6 +3,14 @@
 
 export COMPOSE_PROJECT_NAME=omotes_system_tests
 
-$DOCKER_COMPOSE -f docker-compose.yml -f system_tests/docker-compose.override.yml down -v
-./scripts/setup.sh
-$DOCKER_COMPOSE -f docker-compose.yml -f system_tests/docker-compose.override.yml up --build --abort-on-container-exit
+ENV_FILE=".env.test"
+DOCKER_COMPOSE_FILE="./docker-compose.yml -f system_tests/docker-compose.override.yml"
+
+cp .env-template ${ENV_FILE}
+sed -i 's/LOG_LEVEL=[a-z]*/LOG_LEVEL=WARNING/gi' ${ENV_FILE}
+
+$DOCKER_COMPOSE --env-file ${ENV_FILE} -f $DOCKER_COMPOSE_FILE  down -v
+./scripts/setup_orchestrator_postgres_db.sh $ENV_FILE $DOCKER_COMPOSE_FILE
+./scripts/setup_rabbitmq.sh $ENV_FILE $DOCKER_COMPOSE_FILE
+./scripts/setup_influxdb.sh $ENV_FILE $DOCKER_COMPOSE_FILE
+$DOCKER_COMPOSE --env-file ${ENV_FILE} -f $DOCKER_COMPOSE_FILE up --build --abort-on-container-exit
