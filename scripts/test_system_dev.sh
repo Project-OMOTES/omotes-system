df114a4e72064e55b9b400328196ad600a896d01
Mark Vrijlandt <mark.vrijlandt@tno.nl>
2024-07-05 11:29:10 +0200

    dev start and test for workers

--- scripts/test_system_dev.sh
+++ scripts/test_system_dev.sh
@@ -4,7 +4,7 @@
 export COMPOSE_PROJECT_NAME=omotes_system_tests
 
 ENV_FILE=".env.test"
-DOCKER_COMPOSE_FILE="./docker-compose.yml -f system_tests/docker-compose.override.yml -f system_tests/docker-compose.override.dev.yml"
+DOCKER_COMPOSE_FILE="./docker-compose.yml -f docker-compose.override.dev.yml -f system_tests/docker-compose.override.yml -f system_tests/docker-compose.override.dev.yml"
 
 cp .env-template ${ENV_FILE}
 sed -i 's/LOG_LEVEL=[a-z]*/LOG_LEVEL=WARNING/gi' ${ENV_FILE}
