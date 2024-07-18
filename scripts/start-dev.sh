248856d0f8c4c2f04ddf2630578ee487acc46bd2
Mark Vrijlandt <mark.vrijlandt@tno.nl>
2024-07-18 14:57:59 +0200

    fix double postgres instance with start-dev.sh

--- scripts/start-dev.sh
+++ scripts/start-dev.sh
@@ -3,4 +3,4 @@
 . scripts/_select_docker_compose.sh
 
 $DOCKER_COMPOSE --profile=manual_dev down orchestrator_postgres_db_dev
-$DOCKER_COMPOSE --profile=manual_dev -f docker-compose.yml -f docker-compose.override.dev.yml up --build --wait
+$DOCKER_COMPOSE -f docker-compose.yml -f docker-compose.override.dev.yml up --build --wait
