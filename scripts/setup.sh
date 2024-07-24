0651c3167b444ac8b779ab4b6a8f2ec53c29c9b8
Sebastiaan la Fleur <sebastiaan@lfse.nl>
2024-07-24 11:54:17 +0200

    90: Update setup script to remove available_workflows and request_available_workflows queues to facilicate the change from durable queue to exclusive queue.

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -3,8 +3,6 @@
 ENV_FILE="./.env"
 DOCKER_COMPOSE_FILE="./docker-compose.yml"
 
-set -e
-
 ./scripts/setup_orchestrator_postgres_db.sh $ENV_FILE $DOCKER_COMPOSE_FILE
 ./scripts/setup_rabbitmq.sh $ENV_FILE $DOCKER_COMPOSE_FILE
 ./scripts/setup_influxdb.sh $ENV_FILE $DOCKER_COMPOSE_FILE
--- scripts/setup_rabbitmq.sh
+++ scripts/setup_rabbitmq.sh
@@ -14,3 +14,15 @@ ${DOCKER_EXEC} add_user --vhost omotes "${RABBITMQ_OMOTES_USER_NAME}" "${RABBITM
 ${DOCKER_EXEC} set_permissions --vhost omotes "${RABBITMQ_OMOTES_USER_NAME}" ".*" ".*" ".*"
 ${DOCKER_EXEC} add_user --vhost omotes_celery "${RABBITMQ_CELERY_USER_NAME}" "${RABBITMQ_CELERY_USER_PASSWORD}"
 ${DOCKER_EXEC} set_permissions --vhost omotes_celery "${RABBITMQ_CELERY_USER_NAME}" ".*" ".*" ".*"
+
+
+#_________________________________________________________________________________________________
+# QUEUE MIGRATIONS
+
+# mvp.4.RC2
+# Ticket 84 84-extend-available-workflow-functionality-to-support-multiple-sdks-at-once
+
+# Remove available_workflows & request_available_workflows durable queues if they exist.
+# Messages may be dropped without repercussions.
+${DOCKER_EXEC} delete_queue --vhost omotes available_workflows
+${DOCKER_EXEC} delete_queue --vhost omotes request_available_workflows
