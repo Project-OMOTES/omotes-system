ca801b6ebd33d52d9715ba934366238464653de6
Mark Vrijlandt <mark.vrijlandt@tno.nl>
2024-10-24 14:18:02 +0200

    fix test_system_dev.sh

--- scripts/test_system_dev.sh
+++ scripts/test_system_dev.sh
@@ -10,7 +10,5 @@ cp .env-template ${ENV_FILE}
 sed -i 's/LOG_LEVEL=[a-z]*/LOG_LEVEL=WARNING/gi' ${ENV_FILE}
 
 $DOCKER_COMPOSE --env-file ${ENV_FILE} -f $DOCKER_COMPOSE_FILE  down -v
-./scripts/setup_orchestrator_postgres_db.sh $ENV_FILE $DOCKER_COMPOSE_FILE
-./scripts/setup_rabbitmq.sh $ENV_FILE $DOCKER_COMPOSE_FILE
-./scripts/setup_influxdb.sh $ENV_FILE $DOCKER_COMPOSE_FILE
+./scripts/setup.sh $ENV_FILE "./docker-compose.yml -f ./docker-compose.override.setup.yml"
 $DOCKER_COMPOSE --env-file ${ENV_FILE} -f $DOCKER_COMPOSE_FILE up --build --abort-on-container-exit
